df9e9c43a5f5acc4b970e46469f9ce729c5d410b
Rian Sunthbocus <rtegally1098@gmail.com>
2019-06-02 10:41:25 +0000

    Added the first few pictures and information about poverty around the world, on the homepage and wrote a part of the codes for a world map.

---
+++ assets/js/map.js
@@ -0,0 +1,30 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 1,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    var locations = [{
+        lat: 50.0328308,
+        lng: 8.6947798
+    }, {
+        lat: -33.928992,
+        lng: 18.417396
+    }, {
+        lat: 40.754932,
+        lng: -73.984016
+    }];
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
+return initMap();
