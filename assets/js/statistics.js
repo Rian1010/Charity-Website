4415a07a9e605c5f48ca3f52013ece2bc40d1093
Ubuntu <ubuntu@ip-172-31-43-191.ec2.internal>
2019-07-02 19:36:31 +0000

    Added data of unemployment to the JSON file and tried to make new charts on statistics.html.

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -8,7 +8,9 @@ function createDataVis(error, gdpData) {
     var ndx = crossfilter(gdpData);
 
     show_country_selector(ndx);
-    show_gdps(ndx);
+    show_country_data(ndx);
+
+
 
     dc.renderAll();
 }
@@ -22,7 +24,7 @@ function show_country_selector(ndx) {
         .group(group);
 }
 
-function show_gdps(ndx) {
+function show_country_data(ndx) {
     var name_dim = ndx.dimension(dc.pluck('country'));
     var draw_barchart_gdp_country = name_dim.group().reduceSum(dc.pluck('gdp'));
     dc.barChart('#gdp-per-capita-chart')
@@ -53,14 +55,64 @@ function show_gdps(ndx) {
         .yAxisLabel('Gini Coefficient')
         .yAxis().ticks(8);
 
-    var name_dim = ndx.dimension(dc.pluck('country'));
-    var draw_barchart_gdp_country_pie = name_dim.group().reduceSum(dc.pluck('gini-coefficient'));
-    dc.pieChart('#gdp-per-capita-pie-chart')
+    var unemp_rate_dim = ndx.dimension(dc.pluck('country'));
+    var draw_barchart_unemployment_rate_pie = gc_dim.group().reduceSum(dc.pluck('unemployment-rate'));
+    var minRate = unemp_rate_dim.bottom(1)[0].date;
+    var maxRate = unemp_rate_dim.top(1)[0].date;
+    dc.pieChart('#unemployment-rate-pie-chart')
         .height(330)
         .radius(90)
         .transitionDuration(1500)
-        .dimension(name_dim)
-        .group(draw_barchart_gdp_country_pie);
+        .dimension(unemp_rate_dim)
+        .group(draw_barchart_unemployment_rate_pie);
+
+
+
+
+
+    var unemp_rate_year_dim = ndx.dimension(dc.pluck('country'));
+    var unemp_rate_genders_dim = unemp_rate_year_dim.group().reduceSum(dc.pluck('female-unemployment-rate-2015'));
+
+    function spend_by_country(country) {
+        return function(d) {
+            if (d.country === country) {
+                return +d.country;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+    var femaleUnempRate2015 = unemp_rate_year_dim.group().reduceSum(spend_by_name('female-unemployment-rate-2015'));
+    var femaleUnempRate2016 = unemp_rate_year_dim.group().reduceSum(spend_by_name('female-unemployment-rate-2016'));
+    var femaleUnempRate2017 = unemp_rate_year_dim.group().reduceSum(spend_by_name('female-unemployment-rate-2017'));
+    var femaleUnempRate2018 = unemp_rate_year_dim.group().reduceSum(spend_by_name('female-unemployment-rate-2018'));
+    var femaleUnempRateChart = dc.compositeChart('#unemployment-rate-female');
+
+    compositeChart
+        .width(990)
+        .height(200)
+        .dimension(unemp_rate_year_dim)
+        .x(d3.time.scale().domain())
+        .yAxisLabel("Spend")
+        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+        .renderHorizontalGridLines(true)
+        .compose([
+            dc.lineChart(compositeChart)
+            .colors('green')
+            .group(femUnempRate15, 'female-unemployment-rate-2015'),
+            dc.lineChart(compositeChart)
+            .colors('red')
+            .group(femUnempRate16, 'female-unemployment-rate-2016'),
+            dc.lineChart(compositeChart)
+            .colors('blue')
+            .group(femUnempRate17, 'female-unemployment-rate-2017'),
+            dc.lineChart(compositeChart)
+            .colors('orange')
+            .group(femUnempRate18, 'female-unemployment-rate-2017')
+        ])
+        .brushOn(false)
+        .render();
 }
 /*var name_dim = ndx.dimension(dc.pluck('country'));
 var draw_barchart_gdp_rank = name_dim.group().reduceSum(dc.pluck('rank'));
@@ -112,4 +164,51 @@ dc.barChart('#gdp-per-capita-chart-rank')
                 .xAxisLabel('Country')
                 .yAxisLabel('Gini Index (%)')
                 .yAxis().ticks(4);
+*/
+
+
+
+
+
+
+/*
+DRC F: https://www.theglobaleconomy.com/Democratic-Republic-of-the-Congo/
+
+DRC M: https://www.theglobaleconomy.com/Democratic-Republic-of-the-Congo/Male_unemployment/
+
+MZ F: https://www.theglobaleconomy.com/Mozambique/Female_unemployment/
+
+MZ M: https://www.theglobaleconomy.com/Mozambique/Male_unemployment/
+
+UG F: https://www.theglobaleconomy.com/Uganda/Female_unemployment/
+
+UG M: https://www.theglobaleconomy.com/Uganda/Male_unemployment/
+
+TJ F: https://www.theglobaleconomy.com/Tajikistan/Female_unemployment/
+
+TJ M: https://www.theglobaleconomy.com/Tajikistan/Male_unemployment/
+
+YE F: https://www.theglobaleconomy.com/Yemen/Female_unemployment/
+
+YE M: https://www.theglobaleconomy.com/Yemen/Male_unemployment/
+
+HT F: https://www.theglobaleconomy.com/Tajikistan/Male_unemployment/
+
+HT M: https://www.theglobaleconomy.com/Haiti/Male_unemployment/
+
+ET F: https://www.theglobaleconomy.com/Ethiopia/Female_unemployment/
+
+ET M: https://www.theglobaleconomy.com/Ethiopia/Male_unemployment/
+
+TZ F: https://www.theglobaleconomy.com/Tanzania/Female_unemployment/
+
+TZ M: https://www.theglobaleconomy.com/Tanzania/Male_unemployment/
+
+KG F: https://www.theglobaleconomy.com/Kyrgyzstan/Female_unemployment/
+
+KG M: https://www.theglobaleconomy.com/Tanzania/Male_unemployment/
+
+UZ F: https://www.theglobaleconomy.com/Uzbekistan/Female_unemployment/
+
+UZ M: https://www.theglobaleconomy.com/Uzbekistan/Male_unemployment/
 */
