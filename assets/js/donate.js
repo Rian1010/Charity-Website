de13e5481039e1d8c23082268d6e02e13bd9202d
Rian1010 <rtegally1098@gmail.com>
2019-09-03 17:02:43 +0000

    Working on a draggable calculator.

--- assets/js/donate.js
+++ assets/js/donate.js
@@ -50,250 +50,45 @@ window.onload = function() {
         });
     }
 
-    const btns2 = document.querySelectorAll('.calcBtn2');
-    const equalBtn2 = document.querySelector('.btn-equal2');
-    const clearBtn2 = document.querySelector('.btn-clear2');
-    const screen2 = document.querySelector('.screen2');
+    dragCalculator(document.getElementById("calculatorSection"));
 
+    function dragCalculator(drag) {
+        var pos1 = 0,
+            pos2 = 0,
+            pos3 = 0,
+            pos4 = 0;
+        if (document.getElementById(drag.id + "header")) {
+            document.getElementById(drag.id + "header").onmousedown = dragMouseDown;
+        }
+        else {
+            drag.onmousedown = dragMouseDown;
+        }
 
-    for (let i = 0; i < btns2.length; i++) {
-        btns2[i].addEventListener('click', function() {
-            let number = btns2[i].getAttribute('data-num2');
-            screen2.value += number;
-        });
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragCalculator;
+            document.onmousemove = calculatorDrag;
+        }
 
-        equalBtn2.addEventListener('click', function() {
-            if (screen2.value === '') {
-                alert('Input is empty.');
-            }
-            else {
-                let value = eval(screen2.value);
-                screen2.value = value;
-            }
-        });
+        function calculatorDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
 
-        clearBtn2.addEventListener('click', function() {
-            screen2.value = '';
-        });
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+
+            drag.style.top = (drag.offsetTop - pos2) + "px;";
+            drag.style.left = (drag.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragCalculator() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     }
-
-    const btns3 = document.querySelectorAll('.calcBtn3');
-    const equalBtn3 = document.querySelector('.btn-equal3');
-    const clearBtn3 = document.querySelector('.btn-clear3');
-    const screen3 = document.querySelector('.screen3');
-
-
-    for (let i = 0; i < btns3.length; i++) {
-        btns3[i].addEventListener('click', function() {
-            let number = btns3[i].getAttribute('data-num3');
-            screen3.value += number;
-        });
-
-        equalBtn3.addEventListener('click', function() {
-            if (screen3.value === '') {
-                alert('Input is empty.');
-            }
-            else {
-                let value = eval(screen3.value);
-                screen3.value = value;
-            }
-        });
-
-        clearBtn3.addEventListener('click', function() {
-            screen3.value = '';
-        });
-    }
-
-    const btns4 = document.querySelectorAll('.calcBtn4');
-    const equalBtn4 = document.querySelector('.btn-equal4');
-    const clearBtn4 = document.querySelector('.btn-clear4');
-    const screen4 = document.querySelector('.screen4');
-
-    for (let i = 0; i < btns4.length; i++) {
-        btns4[i].addEventListener('click', function() {
-            let number = btns4[i].getAttribute('data-num4');
-            screen4.value += number;
-        });
-
-        equalBtn4.addEventListener('click', function() {
-            if (screen4.value === '') {
-                alert('Input is empty.');
-            }
-            else {
-                let value = eval(screen4.value);
-                screen4.value = value;
-            }
-        });
-
-        clearBtn4.addEventListener('click', function() {
-            screen4.value = '';
-        });
-    }
-
-    const btns5 = document.querySelectorAll('.calcBtn5');
-    const equalBtn5 = document.querySelector('.btn-equal5');
-    const clearBtn5 = document.querySelector('.btn-clear5');
-    const screen5 = document.querySelector('.screen5');
-
-    for (let i = 0; i < btns5.length; i++) {
-        btns5[i].addEventListener('click', function() {
-            let number = btns5[i].getAttribute('data-num5');
-            screen5.value += number;
-        });
-
-        equalBtn5.addEventListener('click', function() {
-            if (screen5.value === '') {
-                alert('Input is empty.');
-            }
-            else {
-                let value = eval(screen5.value);
-                screen5.value = value;
-                //screenTotal.value5 = value + "+";
-            }
-        });
-
-        clearBtn5.addEventListener('click', function() {
-            screen5.value = '';
-        });
-    }
-
-    const btns6 = document.querySelectorAll('.calcBtn6');
-    const equalBtn6 = document.querySelector('.btn-equal6');
-    const clearBtn6 = document.querySelector('.btn-clear6');
-    const screen6 = document.querySelector('.screen6');
-
-    for (let i = 0; i < btns6.length; i++) {
-        btns6[i].addEventListener('click', function() {
-            let number = btns6[i].getAttribute('data-num6');
-            screen6.value += number;
-        });
-
-        equalBtn6.addEventListener('click', function() {
-            if (screen6.value === '') {
-                alert('Input is empty.');
-            }
-            else {
-                let value = eval(screen6.value);
-                screen6.value = value;
-                //screenTotal6.value = value;
-            }
-        });
-
-        clearBtn6.addEventListener('click', function() {
-            screen6.value = '';
-        });
-    }
-
-    const screenTotal = document.querySelector('.TotalScreen');
-    const screenTotal2 = document.querySelector('.screenTotal2');
-    const equalBtn7 = document.querySelector('.btn-equal7');
-    const clearBtn7 = document.querySelector('.btn-clear7');
-    const clearBtn8 = document.querySelector('.btn-clear8');
-
-    equalBtn.addEventListener('click', function() {
-        let value = eval(screen.value);
-        screenTotal.value += value;
-
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    equalBtn2.addEventListener('click', function() {
-        let value = eval(screen2.value);
-        screenTotal.value += value;
-        
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    equalBtn3.addEventListener('click', function() {
-        let value = eval(screen3.value);
-        screenTotal.value += value;
-        
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    equalBtn4.addEventListener('click', function() {
-        let value = eval(screen4.value);
-        screenTotal.value += value;
-        
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    equalBtn5.addEventListener('click', function() {
-        let value = eval(screen5.value);
-        screenTotal.value += value;
-        
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    equalBtn6.addEventListener('click', function() {
-        let value = eval(screen6.value);
-        screenTotal.value += value;
-        
-        if (screenTotal.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(screenTotal.value);
-            screenTotal.value = value+"+";
-        }
-    });
-
-    /*equalBtn7.addEventListener('click', function() {
-        let value = eval(screenTotal.value);
-        screenTotal2.value += value;
-    });*/
-
-    clearBtn7.addEventListener('click', function() {
-        screenTotal.value = '';
-    });
-    
-    /*clearBtn8.addEventListener('click', function() {
-        screenTotal2.value = '';
-    });*/
-    
-    
-    
-    //var totalScreen = document.querySelector('.TotalScreen');
-    //var totalInput = document.querySelector('.totalInput');
-
-   /* totalScreen.addEventListener('input', function () {
-        if (totalScreen.value === '') {
-            alert('Input is empty.');
-        }
-        else {
-            let value = eval(totalScreen.value);
-            totalScreen.value = value+"+";
-        }
-    });*/
-
 };
