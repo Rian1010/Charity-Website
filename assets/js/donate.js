c39023a561ad7d290ab1ec335c09b94c2dec5679
Rian1010 <rtegally1098@gmail.com>
2019-09-05 09:09:24 +0000

    Edited the textbox of contact.html and a new aside button of donate.html.

--- assets/js/donate.js
+++ assets/js/donate.js
@@ -20,7 +20,24 @@ window.onload = function() {
             }
         });
     }
-    
+
+    //Aside Calculator Button
+    const calculatorBtn = document.querySelector('#calcBtn');
+    const theCalculator = document.querySelector('#calculatorSection')
+
+
+    theCalculator.style.display = 'none';
+
+    calculatorBtn.addEventListener('click', () => {
+        if (theCalculator.style.display == 'none') {
+            theCalculator.style.display = 'block';
+            console.log('this is working');
+        }
+        else {
+            theCalculator.style.display = 'none';
+        }
+    });
+
     //Calculator
 
     const btns = document.querySelectorAll('.calcBtn');
@@ -52,5 +69,61 @@ window.onload = function() {
         });
     }
 
-    
+    dragCalculator(document.getElementById("calculatorSection"));
+
+
+    function dragCalculator(drag) {
+        var pos1 = 0,
+            pos2 = 0,
+            pos3 = 0,
+            pos4 = 0;
+
+        //Where the calculator is moved from 
+        if (document.getElementById(drag.id + "Header")) {
+            document.getElementById(drag.id + "Header").onmousedown = dragMouseDown;
+            console.log(1)
+        }
+
+        //Move the calculator from anywhere else
+        else {
+            drag.onmousedown = dragMouseDown;
+            console.log(2)
+        }
+
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            //Get the startup of the mouseposition
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragCalculator;
+            //Call a function whenever the cursor moves
+            document.onmousemove = calculatorDrag;
+            console.log(3)
+        }
+
+        function calculatorDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+
+            //New cursor position
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+
+            //Set the new position
+            drag.style.top = (drag.offsetTop - pos2) + "px;";
+            drag.style.left = (drag.offsetLeft - pos1) + "px";
+            console.log(4)
+        }
+
+        function closeDragCalculator() {
+            //When the button is released, stop moving
+            document.onmouseup = null;
+            document.onmousemove = null;
+            console.log(5)
+        }
+    }
 };
